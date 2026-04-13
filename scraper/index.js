55cb32fe64c6fb68adb2eaa51efd040280438528
cybersterr <rupanidhruv244@gmail.com>
2026-04-13 13:34:18 +0530

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const STREAM_URL = "https://netx.streamstar18.workers.dev/zio1";
+const STREAM_URL = "https://zio.vodep39240327.workers.dev/";
 const OUTPUT_FILE = "stream.json";
 
 async function fetchAndSaveJson() {
